4464742431ed2d33fae0649c52b7ab7e57f5a865
Yusei Shiozawa <yuyu9000img@gmail.com>
2023-11-16 12:05:22 +0900

    add &mut

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-fn chan(s: &String) -> String {
-   s.clone() + "-chan"
+fn chan(s: &mut String) {
+   s.push_str("-chan");
 }
 
 fn main() {
-    let x = "neko".to_string();
-    let y = chan(&x);
-    println!("{}", y);
+    let mut x = "neko".to_string();
+    chan(&mut x);
     println!("{}", x);
 }
